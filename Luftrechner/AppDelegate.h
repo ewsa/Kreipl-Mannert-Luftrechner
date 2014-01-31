80b7cec378fca85bad5f596ff43a4f50d6dbecff
André Geißelbrecht <maxmustermann@andres-mac-mini.fritz.box>
2014-01-31 17:47:45 +0100

    Initial Commit

---
+++ Luftrechner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Luftrechner
+//
+//  Created by André Geißelbrecht on 31.01.14.
+//  Copyright (c) 2014 André Geißelbrecht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Luftrechner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Luftrechner
+//
+//  Created by André Geißelbrecht on 31.01.14.
+//  Copyright (c) 2014 André Geißelbrecht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
